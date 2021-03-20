babc62a14082dc7d8e43c899f5ec7da9c9da6aa9
vagrant <vagrant@vagrant.vm>
2021-03-20 21:12:03 +0000

    Added functional implementation for checking if both lists are identical

--- further_study.py
+++ further_study.py
@@ -258,8 +258,16 @@ def custom_equality(some_list, another_list):
         False
 
     """
+    #Loop through both strings using for loop
+    #Compare each element at each index in both lists, and keep return value false until both lists are fully traversed
 
-    return None
+    if custom_len(some_list) != custom_len(another_list):
+        return False
+    else:
+        for i in range(custom_len(some_list)):
+            if some_list[i]!=another_list[i]:
+                return False
+    return True
 
 
 # This is the part were we actually run the doctests.
